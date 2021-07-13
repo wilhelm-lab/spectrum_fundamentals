aac20ba391a6e63ac1beb99deee8666cdded3f1a
WassimG <wassim.gabriel@gmail.com>
2021-07-13 16:35:08 +0200

    Added annotation and fixed constants

--- fundamentals/annotation/annotation.py
+++ fundamentals/annotation/annotation.py
@@ -1,6 +1,268 @@
 import pandas as pd
+from joblib import Parallel, delayed
 import numpy as np
 
+from fundamentals import constants
 
-def annotate_spectra(sequence: str, peaks: np):
-    pass
+MIN_CHARGE = 1
+NUM_CORES = 5
+
+
+def maxquant_parser(p):
+    if p[0] == "(":
+        p = p[4] + p[:4] + p[5:]
+    n = len(p)
+    i = 0
+    while i < n:
+        if i < n - 3 and p[i + 1] == "(":
+            j = p[i + 2:].index(")")
+            offset = i + j + 3
+            yield p[i:offset]
+            i = offset
+        else:
+            yield p[i]
+            i += 1
+
+
+def str_to_integer(sequences, parser=maxquant_parser):
+    """
+
+    :param sequences:
+    :param parser:
+    :return:
+    """
+    array = np.zeros([len(sequences), 30], dtype=int)
+    for i, sequence in enumerate(sequences):
+        for j, symbol in enumerate(parser(sequence)):
+            # if symbol == "(":
+            # print(i, j, sequence)
+            try:
+                array[i, j] = constants.ALPHABET[symbol]
+            except Exception:
+                print(sequence)
+    return array
+
+
+def get_modifications(peptide_sequence):
+    """
+    Get modification masses and position in a peptide sequence.
+    :param peptide_sequence: Modified peptide sequence
+    :return: Dict with modification position as an ID and mass as the value.
+    """
+    modification_deltas = {}
+    tmt_n_term = 1
+    if peptide_sequence[:4] == '(U:737)':
+        tmt_n_term = 2
+        modification_deltas.update({0: constants.MOD_MASSES['TMT_6']})
+        peptide_sequence = peptide_sequence[4:]
+
+    count_mod = peptide_sequence.count('(')
+    while '(' in peptide_sequence:
+        if "(" in peptide_sequence:
+            modification_index = peptide_sequence.index('(')
+            if peptide_sequence[modification_index:modification_index + 4] == '(U:737)':
+                if modification_index - 1 in modification_deltas:
+                    modification_deltas.update(
+                        {modification_index - 1: modification_deltas[modification_index - 1] + constants.MOD_MASSES[
+                            'TMT_6']})
+                else:
+                    modification_deltas.update({modification_index - 1: constants.MOD_MASSES['TMT_6']})
+                peptide_sequence = peptide_sequence[0:modification_index] + peptide_sequence[modification_index + 4:]
+                count_mod -= 1
+            elif peptide_sequence[modification_index:modification_index + 4] == '(U:35)':
+                modification_deltas.update({modification_index - 1: constants.ATOM_MASSES['O']})
+                peptide_sequence = peptide_sequence[0:modification_index] + peptide_sequence[modification_index + 4:]
+                count_mod -= 1
+            elif peptide_sequence[modification_index:modification_index + 4] == '(U:21)':
+                modification_deltas.update({modification_index - 1: constants.MOD_MASSES['Phospho']})
+                peptide_sequence = peptide_sequence[0:modification_index] + peptide_sequence[modification_index + 4:]
+                count_mod -= 1
+            elif peptide_sequence[modification_index:modification_index + 4] == '(U:4)':
+                modification_deltas.update({modification_index - 1: constants.MOD_MASSES['Carbomedomethyl']})
+                peptide_sequence = peptide_sequence[0:modification_index] + peptide_sequence[modification_index + 4:]
+                count_mod -= 1
+    return modification_deltas, tmt_n_term, peptide_sequence
+
+
+def initialize_peaks(sequence: str, mass_analyzer: str, charge: int):
+    """
+    Generate theoretical peaks for a modified peptide sequence.
+    :param sequence: Modified peptide sequence.
+    :param mass_analyzer: Type of mass analyzer used eg. FTMS, ITMS
+    :param charge: Precursor charge
+    :return: List of theoretical peaks, Flag to indicate if there is a tmt on n-terminus, Un modified peptide sequence
+    """
+    peptide_sequence = sequence
+    modification_deltas, tmt_n_term, peptide_sequence = get_modifications(peptide_sequence)
+    neutral_losses = []
+    peptide_length = len(peptide_sequence)
+
+    # initialize constants
+    if int(round(charge)) <= 3:
+        max_charge = int(round(charge))
+    else:
+        max_charge = 3
+
+    n_term_delta = 0.0
+
+    # get mass delta for the c-terminus
+    c_term_delta = 0.0
+
+    n_term = constants.ATOM_MASSES['H'] + n_term_delta  # n-terminal delta [N]
+    c_term = constants.ATOM_MASSES['O'] + constants.ATOM_MASSES[
+        'H'] + c_term_delta  # c-terminal delta [C]
+
+    cho = constants.ATOM_MASSES['H'] + constants.ATOM_MASSES['C'] + constants.ATOM_MASSES[
+        'O']
+    h = constants.ATOM_MASSES['H']
+    co = constants.ATOM_MASSES['C'] + constants.ATOM_MASSES['O']
+    nh2 = constants.ATOM_MASSES['N'] + constants.ATOM_MASSES['H'] * 2.0
+
+    ion_type_offsets = [n_term - h, c_term + h]
+
+    # tmp place holder
+    ion_type_masses = [0, 0]
+    ion_types = ["b", "y"]
+
+    number_of_ion_types = len(ion_type_offsets)
+    fragments_meta_data = []
+    # calculation:
+    forward_sum = 0.0  # sum over all amino acids from left to right (neutral charge)
+    backward_sum = 0.0  # sum over all amino acids from right to left (neutral charge)
+    added_sequence = False
+    for i in range(0, peptide_length):  # generate substrings
+        forward_sum += constants.AA_MASSES[peptide_sequence[i]]  # sum left to right
+        if i in modification_deltas:  # add mass of modification if present
+            forward_sum += modification_deltas[i]
+        backward_sum += constants.AA_MASSES[peptide_sequence[peptide_length - i - 1]]  # sum right to left
+        if peptide_length - i - 1 in modification_deltas:  # add mass of modification if present
+            backward_sum += modification_deltas[peptide_length - i - 1]
+
+        ion_type_masses[0] = forward_sum + ion_type_offsets[0]  # b ion - ...
+
+        ion_type_masses[1] = backward_sum + ion_type_offsets[1]  # y ion
+
+        for charge in range(MIN_CHARGE, max_charge + 1):  # generate ion in different charge states
+            # positive charge is introduced by protons (or H - ELECTRON_MASS)
+            charge_delta = charge * constants.PROTON_MASS
+            for ion_type in range(0, number_of_ion_types):  # generate all ion types
+                # Check for neutral loss here
+                mass = (ion_type_masses[ion_type] + charge_delta) / charge
+                if mass_analyzer == 'FTMS':
+                    min_mass = (mass * -20 / 1000000) + mass
+                    max_mass = (mass * 20 / 1000000) + mass
+                else:
+                    min_mass = mass - 0.5
+                    max_mass = mass + 0.5
+                fragments_meta_data.append({'ion_type': ion_types[ion_type], 'no': i + 1, 'charge': charge,
+                                            'theoretical_mass': mass, 'min_mass': min_mass, 'max_mass': max_mass})
+        fragments_meta_data = sorted(fragments_meta_data, key=lambda d: d['theoretical_mass'])
+    return fragments_meta_data, tmt_n_term, peptide_sequence
+
+
+def match_peaks(fragments_meta_data: list, peaks_intensity: np,
+                peaks_masses: np, tmt_n_term: int, unmod_sequence: str, charge: int):
+    start_peak = 0
+    no_of_peaks = len(peaks_intensity)
+    max_intensity = 1
+    row_list = []
+    temp_list = []
+    for fragment in fragments_meta_data:
+        min_mass = fragment['min_mass']
+        max_mass = fragment['max_mass']
+        fragment_no = fragment['no']
+        while start_peak < no_of_peaks:
+            peak_mass = peaks_masses[start_peak]
+            peak_intensity = peaks_intensity[start_peak]
+
+            if peak_mass > max_mass:
+                break
+            if peak_mass < min_mass:
+                for row in row_list:
+                    row['intensity'] = float(row['intensity']) / max_intensity
+                    temp_list.append(row)
+                return temp_list
+            if min_mass <= peak_mass <= max_mass:
+                if fragment['ion_type'] == 'b' and fragment_no == 1:
+                    if (unmod_sequence[0] == 'R' or unmod_sequence[0] == 'H' or unmod_sequence[0] == 'K') and (
+                            tmt_n_term == 1):
+                        row_list.append(
+                            {'ion_type': fragment['ion_type'], 'no': fragment_no, 'charge': charge,
+                             'exp_mass': peak_mass, 'intensity': peak_intensity})
+                        if peak_intensity > max_intensity:
+                            max_intensity = float(peak_intensity)
+                else:
+                    row_list.append(
+                        {'ion_type': fragment['ion_type'], 'no': fragment_no, 'charge': charge,
+                         'exp_mass': peak_mass, 'intensity': peak_intensity})
+                    if peak_intensity > max_intensity:
+                        max_intensity = float(peak_intensity)
+        for row in row_list:
+            row['intensity'] = float(row['intensity']) / max_intensity
+            temp_list.append(row)
+    return temp_list
+
+
+def annotate_spectra(un_annot_spectra: pd.DataFrame):
+    spectra_rows = []
+    raw_file_annotations = []
+    for row in un_annot_spectra.values:
+        spectra_rows.append(row)
+        if len(spectra_rows) % 2400 == 0:
+            results = Parallel(n_jobs=NUM_CORES)(delayed(parallel_annotate)(row) for row in spectra_rows)
+            flatten = [item for sublist in results for item in sublist]
+            raw_file_annotations.append(flatten)
+            spectra_rows = []
+
+    results = Parallel(n_jobs=NUM_CORES)(delayed(parallel_annotate)(row) for row in spectra_rows)
+    flatten = [item for sublist in results for item in sublist]
+    raw_file_annotations.append(flatten)
+    flatten = [item for sublist in raw_file_annotations for item in sublist]
+    return raw_file_annotations
+
+
+def generate_annotation_matrix(matched_peaks, unmod_seq: str, charge: int):
+    intensity = np.full(174, -1.0)
+    mass = np.full(174, -1.0)
+
+    # change values to zeros
+    if len(unmod_seq) < 30:
+        peaks_range = range(0, (len(unmod_seq) - 1) * 6)
+    else:
+        peaks_range = range(0, (len(unmod_seq) - 1) * 6)
+    if charge == 1:
+        available_peaks = [index for index in peaks_range if (index % 3 == 0)]
+    elif charge == 2:
+        available_peaks = [index for index in peaks_range if (index % 3 in (0, 1))]
+    else:
+        available_peaks = [index for index in peaks_range]
+
+    intensity[available_peaks] = 0.0
+
+    mass[available_peaks] = 0.0
+    for peak in matched_peaks:
+        if peak['ion_type'] == 'y':
+            peak_pos = (peak['no'] - 1) * 6 + (peak['charge'] - 1)
+        else:
+            peak_pos = (peak['no'] - 1) * 6 + (peak['charge'] - 1) + 3
+
+        intensity[peak_pos] = peak['intensity']
+        mass[peak_pos] = peak['exp_mass']
+
+    if len(unmod_seq) < 30:
+        mask_peaks = range((len(unmod_seq) - 1) * 6, ((len(unmod_seq) - 1) * 6) + 6)
+        intensity[mask_peaks] = -1.0
+        mass[mask_peaks] = -1.0
+
+    return intensity, mass
+
+
+def parallel_annotate(row):
+    fragments_meta_data, tmt_n_term, unmod_sequence = initialize_peaks(row['MODIFIED_SEQUENCE'], row['MASS_ANALYZER'],
+                                                                       row['CHARGE'])
+    matched_peaks = match_peaks(fragments_meta_data, row['intensity'], row['mz'], tmt_n_term, unmod_sequence,
+                                row['CHARGE'])
+    intensities, mass = generate_annotation_matrix(matched_peaks, unmod_sequence, row['CHARGE'])
+    row['intensity'] = intensities
+    row['mz'] = mass
+    return row
--- fundamentals/constants.py
+++ fundamentals/constants.py
@@ -1,6 +1,6 @@
-ALPHABET = {
+AA_ALPHABET = {
     "A": 1,
-    "C": 2,
+    "C": 23,
     "D": 3,
     "E": 4,
     "F": 5,
@@ -19,12 +19,27 @@ ALPHABET = {
     "V": 18,
     "W": 19,
     "Y": 20,
-    "M(ox)": 21,
-    "K(tm)": 22,
-    "S(ph)": 25,
-    "T(ph)": 26,
-    "Y(ph)": 27,
 }
+ALPHABET_MODS = {
+    "M(U:35)": 21,
+    "C(U:4)": 2,  ## TODO check why this has the same value as C
+    "K(U:737)": 22,
+    "S(U:21)": 25,
+    "T(U:21)": 26,
+    "Y(U:21)": 27
+}
+ALPHABET = {**AA_ALPHABET, **ALPHABET_MODS}
+
+MAXQUANT_VAR_MODS = {
+    "M(ox)": "M(U:35)",
+    "M(Oxidation (M))": "M(U:35)",
+    "K(tm)": "K(U:737)",
+    "S(ph)": "S(U:21)",
+    "T(ph)": "T(U:21)",
+    "Y(ph)": "Y(U:21)",
+    "C(??)": "C(U:4)", # TODO Carbamidomethyl ##
+}
+
 
 AA_MASSES = {
     'A': 71.037114,
@@ -53,11 +68,14 @@ AA_MASSES = {
 MOD_MASSES = {
     'TMT_6': 229.162932,
     'Phospho': 79.966331,
+    'Carbomedomethyl': 57.02146,
 }
 
 # initialize other masses
 PROTON_MASS = 1.007276467
 ELECTRON_MASS = 0.00054858
+
+EPSILON = 1e-7
 
 # masses of different atoms
 ATOM_MASSES = {
@@ -66,3 +84,13 @@ ATOM_MASSES = {
     'O': 15.9949146,
     'N': 14.003074,
 }
+
+Meta_Data_Columns=['RAW_FILE',
+                   'SCAN_NUMBER',
+                   'MODIFIED_SEQUENCE',
+                   'CHARGE',
+                    'FRAGMENTATION',
+                   'MASS_ANALYZER',
+                   'MASS',
+                   'SCORE',
+                   'REVERSE']
