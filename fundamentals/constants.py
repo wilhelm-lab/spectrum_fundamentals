414c1f1704b9814f320775e0328587c80fcbedf1
Mario Picciani <mario.picciani@tum.de>
2021-07-14 13:13:26 +0200

    added MZML_DATA_COLUMNS

--- fundamentals/constants.py
+++ fundamentals/constants.py
@@ -96,12 +96,14 @@ NUM_IONS = (MAX_PEPTIDE_LEN - 1)*2*3
 B_ION_MASK = np.tile([0,0,0,1,1,1], MAX_PEPTIDE_LEN - 1)
 Y_ION_MASK = np.tile([1,1,1,0,0,0], MAX_PEPTIDE_LEN - 1)
 
-Meta_Data_Columns=['RAW_FILE',
-                   'SCAN_NUMBER',
-                   'MODIFIED_SEQUENCE',
+META_DATA_ONLY_COLUMNS = ['MODIFIED_SEQUENCE',
                    'CHARGE',
                    'FRAGMENTATION',
                    'MASS_ANALYZER',
                    'MASS',
                    'SCORE',
                    'REVERSE']
+SHARED_DATA_COLUMNS = ['RAW_FILE', 'SCAN_NUMBER']
+Meta_Data_Columns = SHARED_DATA_COLUMNS + META_DATA_ONLY_COLUMNS
+MZML_ONLY_DATA_COLUMNS = ['INTENSITIES', 'MZ']
+MZML_DATA_COLUMNS =  SHARED_DATA_COLUMNS + MZML_ONLY_DATA_COLUMNS
