22d52a74651710d88ad9ff767b21b2be6d0a3402
Matthew The <matthew.the@tum.de>
2021-07-14 16:32:15 +0200

    Added percolator base features

--- fundamentals/metrics/percolator.py
+++ fundamentals/metrics/percolator.py
@@ -1,21 +1,108 @@
-from metric import Metric
+import hashlib
+
+import numpy as np
+import pandas as pd
+
+from .metric import Metric
 
 
 class Percolator(Metric):
-
+    """
+    Expects the following metadata columns:
+    RAW_FILE
+    SCAN_NUMBER
+    MODIFIED_SEQUENCE
+    CHARGE
+    MASS
+    CALCULATED_MASS
+    SCORE
+    REVERSE
+    FRAGMENTATION
+    MASS_ANALYZER
+    """
+    metadata: pd.DataFrame
+    
+    def __init__(self, metadata, pred_intensities, true_intensities):
+        self.metadata = metadata
+        super().__init__(pred_intensities, true_intensities)
+        
     def fit_loess(self):
         """
         Use IRT value here
         """
-        pass
+        
 
     @staticmethod
-    def get_scannr(rawfile, scannr):
-        s = "{}{}".format(rawfile, scannr).encode()
+    def get_scannr(metadata_subset):
+        raw_file, scan_number = metadata_subset
+        s = "{}{}".format(raw_file, scan_number).encode()
         return int(hashlib.sha224(s).hexdigest()[:12], 16)
+    
+    @staticmethod
+    def get_specid(metadata_subset):
+        raw_file, scan_number, modified_sequence, charge = metadata_subset
+        s = "{}-{}-{}-{}".format(
+            raw_file, scan_number, modified_sequence, charge
+        )
+        return s
+    
+    @staticmethod
+    def count_missed_cleavages(sequence):
+        return sequence[:-1].count("K") + sequence[:-1].count("R")
+    
+    @staticmethod
+    def count_arginines_and_lysines(sequence):
+        return sequence.count("K") + sequence.count("R")
+    
+    @staticmethod
+    def calculate_mass_difference(metadata_subset):
+        experimental_mass, calculated_mass = metadata_subset
+        return calculated_mass - experimental_mass
+    
+    @staticmethod
+    def calculate_mass_difference_ppm(metadata_subset):
+        experimental_mass, calculated_mass = metadata_subset
+        return (calculated_mass - experimental_mass) / experimental_mass * 1e6
+        
+    @staticmethod
+    def get_target_decoy_label(reverse):
+        """
+        :return: target/decoy label for percolator, 1 = Target, 0 = Decoy
+        """
+        return 0 if reverse else 1
 
     def calc(self):
         """
-        Here we should calculate all metrics
+        Calculate percolator metadata and feature columns from PSM metadata
         """
-        pass
+        
+        # PSM metadata
+        self.metrics_val['SpecId'] = self.metadata[['RAW_FILE', 'SCAN_NUMBER', 'MODIFIED_SEQUENCE', 'CHARGE']].apply(Percolator.get_specid, axis=1)
+        self.metrics_val['Label'] = self.metadata['REVERSE'].apply(Percolator.get_target_decoy_label)
+        self.metrics_val['ScanNr'] = self.metadata[['RAW_FILE', 'SCAN_NUMBER']].apply(Percolator.get_scannr, axis = 1)
+        self.metrics_val['ExpMass'] = self.metadata['MASS']
+        self.metrics_val['Peptide'] = self.metadata['MODIFIED_SEQUENCE']
+        self.metrics_val['Protein'] = self.metadata['MODIFIED_SEQUENCE'] # we don't need the protein ID to get PSM / peptide results, fill with peptide sequence
+        
+        # PSM features
+        self.metrics_val['missedCleavages'] = self.metadata['MODIFIED_SEQUENCE'].apply(Percolator.count_missed_cleavages)
+        self.metrics_val['KR'] = self.metadata['MODIFIED_SEQUENCE'].apply(Percolator.count_arginines_and_lysines)
+        self.metrics_val['sequence_length'] = self.metadata['MODIFIED_SEQUENCE'].apply(lambda x : len(x)) # TODO: remove modifications
+        
+        self.metrics_val['Mass'] = self.metadata['MASS'] # this is the experimental mass used as a feature
+        self.metrics_val['deltaM_Da'] = self.metadata[['MASS', 'CALCULATED_MASS']].apply(Percolator.calculate_mass_difference, axis = 1)
+        self.metrics_val['absDeltaM_Da'] = np.abs(self.metrics_val['deltaM_Da'])
+        self.metrics_val['deltaM_ppm'] = self.metadata[['MASS', 'CALCULATED_MASS']].apply(Percolator.calculate_mass_difference_ppm, axis = 1)
+        self.metrics_val['absDeltaM_ppm'] = np.abs(self.metrics_val['deltaM_ppm'])
+        
+        self.metrics_val['Charge2'] = (self.metadata['CHARGE'] == 2).astype(int)
+        self.metrics_val['Charge3'] = (self.metadata['CHARGE'] == 3).astype(int)
+        
+        self.metrics_val['UnknownFragmentationMethod'] = (~self.metadata['FRAGMENTATION'].isin(['HCD', 'CID'])).astype(int)
+        self.metrics_val['HCD'] = (self.metadata['FRAGMENTATION'] == 'HCD').astype(int)
+        self.metrics_val['CID'] = (self.metadata['FRAGMENTATION'] == 'CID').astype(int)
+        
+        self.metrics_val['RT'] = self.metadata['RT']
+        self.metrics_val['pred_RT'] = self.metadata['PREDICTED_RT']
+        self.metrics_val['abs_rt_diff'] = self.metadata['RT'] - self.metadata['PREDICTED_RT']
+        
--- tests/unit_tests/test_percolator.py
+++ tests/unit_tests/test_percolator.py
@@ -7,40 +7,84 @@ import fundamentals.constants as constants
 
 
 class TestPercolator:
-
+    def test_get_scannr(self):
+        np.testing.assert_equal(perc.Percolator.get_scannr(('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02', 7978)), 171184297275363)
+    
+    def test_get_specid(self):
+        np.testing.assert_string_equal(perc.Percolator.get_specid(('rawfile', 1234, 'ABCD', 2)), 'rawfile-1234-ABCD-2')
+    
+    def test_count_missed_cleavages(self):
+        np.testing.assert_equal(perc.Percolator.count_missed_cleavages('AKAAAAKAK'), 2)
+    
+    def test_count_arginines_and_lysines(self):
+        np.testing.assert_equal(perc.Percolator.count_arginines_and_lysines('ARAAAAKAK'), 3)
+    
+    def test_calculate_mass_difference(self):
+        np.testing.assert_almost_equal(perc.Percolator.calculate_mass_difference((1000.0, 1001.2)), 1.2)
+    
+    def test_calculate_mass_difference_ppm(self):
+        np.testing.assert_almost_equal(perc.Percolator.calculate_mass_difference_ppm((1000.0, 1001.2)), 1200.0)
+    
+    def test_get_target_decoy_label_target(self):
+        reverse = False
+        np.testing.assert_equal(perc.Percolator.get_target_decoy_label(reverse), 1)
+    
+    def test_get_target_decoy_label_decoy(self):
+        reverse = True
+        np.testing.assert_equal(perc.Percolator.get_target_decoy_label(reverse), 0)
+        
     def test_calc(self):
-        cols = ['RAW_FILE', 'SCAN_NUMBER', 'MODIFIED_SEQUENCE', 'CHARGE', 'MASS', 'SCORE', 'REVERSE', 'FRAGMENTATION', 'MASS_ANALYZER']
+        cols = ['RAW_FILE', 'SCAN_NUMBER', 'MODIFIED_SEQUENCE', 'CHARGE', 'MASS', 'CALCULATED_MASS', 'SCORE', 'REVERSE', 'FRAGMENTATION', 'MASS_ANALYZER']
         perc_input = pd.DataFrame(columns=cols)
-        perc_input = perc_input.append(pd.Series(
-            '20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,7978,AAIGEATRL,2,900.5028800000001,60.43600000000001,False,HCD,FTMS'.split(','),
-            index=cols), ignore_index=True)
         perc_input = perc_input.append(
-            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,12304,AAVPRAAFL,2,914.53379,94.006,False,HCD,FTMS'.split(','),
-                      index=cols), ignore_index=True)
-        perc_input = perc_input.append(pd.Series(
-            '20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,12398,AAYFGVYDTAK,2,1204.5764,79.97399999999999,False,HCD,FTMS'.split(','),
-            index=cols), ignore_index=True)
-        perc_input = perc_input.append(
-            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,11716,AAYYHPSYL,2,1083.5025,99.919,False,HCD,FTMS'.split(','),
+            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,7978,AAIGEATRL,2,900.50345678,900.50288029264,60.43600000000001,False,HCD,FTMS'.split(','),
                       index=cols), ignore_index=True)
         perc_input = perc_input.append(
-            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,5174,AEDLNTRVA,2,987.49852,99.802,False,HCD,FTMS'.split(','),
+            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,12304,AAVPRAAFL,2,914.53379,914.53379,94.006,True,HCD,FTMS'.split(','),
                       index=cols), ignore_index=True)
-
+        perc_input = perc_input.append(
+            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,12398,AAYFGVYDTAK,2,1204.5764,1204.5764,79.97399999999999,False,HCD,FTMS'.split(','),
+                      index=cols), ignore_index=True)
+        perc_input = perc_input.append(
+            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,11716,AAYYHPSYL,2,1083.5025,1083.5025,99.919,False,HCD,FTMS'.split(','),
+                      index=cols), ignore_index=True)
+        perc_input = perc_input.append(
+            pd.Series('20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02,5174,AEDLNTRVA,2,987.49852,987.49852,99.802,False,HCD,FTMS'.split(','),
+                      index=cols), ignore_index=True)
+        perc_input['SCAN_NUMBER'] = perc_input['SCAN_NUMBER'].astype(int)
+        perc_input['CHARGE'] = perc_input['CHARGE'].astype(int)
+        perc_input['MASS'] = perc_input['MASS'].astype(float)
+        perc_input['CALCULATED_MASS'] = perc_input['CALCULATED_MASS'].astype(float)
+        perc_input['REVERSE'] = perc_input['REVERSE'] == "True"
+        
+        predicted_intensities = [[]]
+        observed_intensities = [[]]
         percolator = perc.Percolator(perc_input, predicted_intensities, observed_intensities)
         percolator.calc()
 
-        # counting metrics
+        # meta data for percolator
         np.testing.assert_string_equal(percolator.metrics_val['SpecId'][0], '20210122_0263_TMUCLHan_Peiru_DDA_IP_C797S_02-7978-AAIGEATRL-2')
+        np.testing.assert_equal(percolator.metrics_val['Label'][0], 1)
         np.testing.assert_equal(percolator.metrics_val['ScanNr'][0], 171184297275363)
+        np.testing.assert_almost_equal(percolator.metrics_val['ExpMass'][0], 900.50345678)
         np.testing.assert_string_equal(percolator.metrics_val['Peptide'][0], 'AAIGEATRL')
+        np.testing.assert_string_equal(percolator.metrics_val['Protein'][0], 'AAIGEATRL') # we don't need the protein ID to get PSM / peptide results
+        
+        # features
         np.testing.assert_equal(percolator.metrics_val['missedCleavages'][0], 1)
+        np.testing.assert_equal(percolator.metrics_val['KR'][0], 1)
         np.testing.assert_equal(percolator.metrics_val['sequence_length'][0], 9)
-        np.testing.assert_almost_equal(percolator.metrics_val['Mass'][0], 900.50288)
-        np.testing.assert_string_equal(percolator.metrics_val['ExpMass'][0], 900.50288)
-        np.testing.assert_string_equal(percolator.metrics_val['Label'][0], 1)
-        np.testing.assert_string_equal(percolator.metrics_val['Protein'][0], 'AAIGEATRL')
-        np.testing.assert_string_equal(percolator.metrics_val['deltaM_ppm'][0], '') # 900.492
-
-
+        np.testing.assert_almost_equal(percolator.metrics_val['Mass'][0], 900.50345678) # this is the experimental mass as a feature
+        np.testing.assert_almost_equal(percolator.metrics_val['deltaM_Da'][0], -0.0005764873)
+        np.testing.assert_almost_equal(percolator.metrics_val['absDeltaM_Da'][0], 0.0005764873)
+        np.testing.assert_almost_equal(percolator.metrics_val['deltaM_ppm'][0], -0.64018339472)
+        np.testing.assert_almost_equal(percolator.metrics_val['absDeltaM_ppm'][0], 0.64018339472)
+        np.testing.assert_equal(percolator.metrics_val['Charge2'][0], 1)
+        np.testing.assert_equal(percolator.metrics_val['Charge3'][0], 0)
+        np.testing.assert_equal(percolator.metrics_val['UnknownFragmentationMethod'][0], 0)
+        np.testing.assert_equal(percolator.metrics_val['HCD'][0], 1)
+        np.testing.assert_equal(percolator.metrics_val['CID'][0], 0)
+        
+        # check label of second PSM (decoy)
+        np.testing.assert_equal(percolator.metrics_val['Label'][1], 0)
 
