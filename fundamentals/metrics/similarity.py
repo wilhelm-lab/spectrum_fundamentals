93b8c2c8cbd329d84f25912af093a32fbab3821d
Matthew The <matthew.the@tum.de>
2021-07-14 11:48:28 +0200

    Added support for spectral angle with matrices of intensities

--- fundamentals/metrics/similarity.py
+++ fundamentals/metrics/similarity.py
@@ -15,39 +15,63 @@ class SimilarityMetrics(Metric):
         """
         epsilon = 1e-7
         
-        observed_peaks = np.argwhere(observed_intensities > constants.EPSILON)
-        predicted_peaks = np.argwhere(predicted_intensities > constants.EPSILON)
+        if False:
+            observed_peaks = np.argwhere(observed_intensities > constants.EPSILON)
+            predicted_peaks = np.argwhere(predicted_intensities > constants.EPSILON)
+            
+            print(observed_peaks)
+            print(predicted_peaks)
+            not_both_zero = np.union1d(observed_peaks, predicted_peaks)
+            print(not_both_zero)
+            if len(not_both_zero) == 0:
+                return 0.0
+            
+            observed_masked = observed_intensities[not_both_zero]
+            predicted_masked = predicted_intensities[not_both_zero]
+            
+            observed_masked += epsilon
+            predicted_masked += epsilon
+        else:
+            observed_masked = observed_intensities
+            predicted_masked = predicted_intensities
         
-        not_both_zero = np.union1d(observed_peaks, predicted_peaks)
+        observed_normalized = SimilarityMetrics.unit_normalization(observed_masked)
+        predicted_normalized = SimilarityMetrics.unit_normalization(predicted_masked)
         
-        if len(not_both_zero) == 0:
-            return 0.0
-        
-        observed_masked = observed_intensities[not_both_zero]
-        predicted_masked = predicted_intensities[not_both_zero]
-        
-        observed_masked += epsilon
-        predicted_masked += epsilon
-        
-        observed_normalized = normalize_vector(observed_masked)
-        predicted_normalized = normalize_vector(predicted_masked)
-        
-        dot_product = np.sum(observed_normalized * predicted_normalized, axis=0)
+        dot_product = np.sum(observed_normalized * predicted_normalized, axis = 1)
         
         arccos = np.arccos(dot_product)
         return 1 - 2 * arccos / np.pi
     
     @staticmethod
-    def l2_norm(vector):
-        # = np.sqrt(np.sum(np.square(vector), axis=0))
-        return np.linalg.norm(vector, axis=0)
+    def l2_norm(matrix):
+        """
+        compute the l2-norm ( sqrt(sum(x^2) ) for each row of the matrix
+        :param matrix: matrix with intensities, constants.EPSILON intensity indicates zero intensity peaks, 0 intensity indicates invalid peaks (charge state > peptide charge state or position >= peptide length), matrix of size (nspectra, 174)
+        :return: vector with rowwise norms of the matrix
+        """
+        # = np.sqrt(np.sum(np.square(matrix), axis=0))
+        return np.linalg.norm(matrix, axis=1)
     
     @staticmethod
-    def normalize_vector(vector):
-        return vector / SimilarityMetrics.l2_norm(vector)
+    def unit_normalization(matrix):
+        """
+        normalize each row of the matrix such that the norm equals 1.0
+        :param matrix: matrix with intensities, constants.EPSILON intensity indicates zero intensity peaks, 0 intensity indicates invalid peaks (charge state > peptide charge state or position >= peptide length), matrix of size (nspectra, 174)
+        :return: normalized matrix
+        """
+        rowwise_norm = SimilarityMetrics.l2_norm(matrix)
+        # prevent divide by zero
+        rowwise_norm[rowwise_norm == 0] = 1 
+        return matrix / rowwise_norm[:, np.newaxis]
     
     def pearson(self):
         pass
 
     def calc(self):
-        pass
+        """
+        Adds columns with spectral angle feature to metrics_val dataframe
+        """
+        
+        self.metrics_val['spectral_angle'] = SimilarityMetrics.spectral_angle(self.true_intensities, self.pred_intensities)
+        
--- tests/unit_tests/test_similarity.py
+++ tests/unit_tests/test_similarity.py
@@ -8,52 +8,80 @@ import fundamentals.constants as constants
 class TestSpectralAngle:
     
     def test_l2_norm(self):
-        vector = np.array([1,2,3,4], dtype = 'float64')
+        vector = get_padded_array([1.0, 2.0, 3.0, 4.0])
         np.testing.assert_equal(sim.SimilarityMetrics.l2_norm(vector), np.sqrt(30))
     
     def test_spectral_angle_equal(self):
-        vector1 = np.array([1,2,3,4], dtype = 'float64')
-        vector2 = np.array([1,2,3,4], dtype = 'float64')
+        vector1 = get_padded_array([1.0, 2.0, 3.0, 4.0])
+        vector2 = get_padded_array([1.0, 2.0, 3.0, 4.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 1.0)
     
     def test_spectral_angle_equal_scaled(self):
-        vector1 = np.array([1,2,3,4], dtype = 'float64')
-        vector2 = np.array([2,4,6,8], dtype = 'float64')
+        vector1 = get_padded_array([1.0, 2.0, 3.0, 4.0])
+        vector2 = get_padded_array([2.0, 4.0, 6.0, 8.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 1.0)
     
     def test_spectral_angle_zero(self):
         z = constants.EPSILON
-        vector1 = np.array([z,2,z,4], dtype = 'float64')
-        vector2 = np.array([1,z,3,z], dtype = 'float64')
+        vector1 = get_padded_array([  z, 2.0,   z, 4.0])
+        vector2 = get_padded_array([1.0,   z, 3.0,   z])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.0)
     
     def test_spectral_angle_all_zero(self):
         z = constants.EPSILON
-        vector1 = np.array([z,z,z,z], dtype = 'float64')
-        vector2 = np.array([z,z,z,z], dtype = 'float64')
+        vector1 = get_padded_array([z,z,z,z])
+        vector2 = get_padded_array([z,z,z,z])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.0)
         
     def test_spectral_angle_invalid(self):
-        vector1 = np.array([0,2,0,4], dtype = 'float64')
-        vector2 = np.array([1,0,3,0], dtype = 'float64')
+        vector1 = get_padded_array([0.0, 2.0, 0.0, 4.0])
+        vector2 = get_padded_array([1.0, 0.0, 3.0, 0.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.0)
     
     def test_spectral_angle_full(self):
         # 1 - 2*arccos(28/30)/pi
-        vector1 = np.array([1,2,4,3], dtype = 'float64')
-        vector2 = np.array([2,1,3,4], dtype = 'float64')
+        vector1 = get_padded_array([1.0, 2.0, 4.0, 3.0])
+        vector2 = get_padded_array([2.0, 1.0, 3.0, 4.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.76622811354)
     
     def test_spectral_angle_full_with_zeros(self):
         # 1 - 2*arccos(24/29)/pi
         z = constants.EPSILON
-        vector1 = np.array([z,2,4,3], dtype = 'float64')
-        vector2 = np.array([2,z,3,4], dtype = 'float64')
+        vector1 = get_padded_array([  z, 2.0, 4.0, 3.0])
+        vector2 = get_padded_array([2.0,   z, 3.0, 4.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.62057306283)
 
     def test_spectral_angle_full_with_both_zeros(self):
         # 1 - 2*arccos(12/25)/pi
         z = constants.EPSILON
-        vector1 = np.array([z,3,4,z], dtype = 'float64')
-        vector2 = np.array([z,z,3,4], dtype = 'float64')
+        vector1 = get_padded_array([  z, 3.0, 4.0,   z])
+        vector2 = get_padded_array([  z,   z, 3.0, 4.0])
         np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(vector1, vector2), 0.31872668904)
+
+class TestSpectralAngleMultiRow:
+    
+    def test_l2_norm(self):
+        vector1 = get_padded_array([1.0, 2.0, 3.0, 4.0])
+        vector2 = get_padded_array([1.0, 2.0, 3.0, 5.0])
+        matrix = np.concatenate((vector1, vector2), axis = 0)
+        np.testing.assert_equal(sim.SimilarityMetrics.l2_norm(matrix), np.array([np.sqrt(30), np.sqrt(39)]))
+    
+    def test_spectral_angle_full(self):
+        z = constants.EPSILON
+        vector1 = get_padded_array([1.0, 2.0, 4.0, 3.0])
+        vector2 = get_padded_array([  z, 3.0, 4.0,   z])
+        vector3 = get_padded_array([  z,   z,   z,   z])
+        vector4 = get_padded_array([0.0, 0.0, 0.0, 0.0])
+        matrix1 = np.concatenate((vector1, vector2, vector3, vector4), axis = 0)
+        
+        vector1 = get_padded_array([2.0, 1.0, 3.0, 4.0]) # 1 - 2*arccos(28/30)/pi
+        vector2 = get_padded_array([  z,   z, 3.0, 4.0]) # 1 - 2*arccos(12/25)/pi
+        vector3 = get_padded_array([  z,   z,   z,   z])
+        vector4 = get_padded_array([0.0, 0.0, 0.0, 0.0])
+        matrix2 = np.concatenate((vector1, vector2, vector3, vector4), axis = 0)
+        
+        np.testing.assert_almost_equal(sim.SimilarityMetrics.spectral_angle(matrix1, matrix2), np.array([0.76622811354, 0.31872668904, 0.0, 0.0]))
+    
+        
+def get_padded_array(l, padding_value = 0):
+    return np.array([np.pad(l, (0, constants.NUM_IONS - len(l)), 'constant', constant_values=padding_value)])
