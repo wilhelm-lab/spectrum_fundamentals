cdf78ac0c2fa4a096bab5ee80b86deb152989bcb
Matthew The <matthew.the@tum.de>
2021-11-18 17:55:43 +0100

    Split stderr and stdout logging

--- fundamentals/__init__.py
+++ fundamentals/__init__.py
@@ -1,8 +1,11 @@
+import sys
+import logging
+import logging.handlers
+import time
+
 from .constants import *
 from . import mod_string
 
-import logging.handlers
-import time
 
 CONSOLE_LOG_LEVEL = logging.INFO
 logger = logging.getLogger(__name__)
@@ -11,9 +14,15 @@ if len(logger.handlers) == 0:
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s::%(funcName)s %(message)s")
     formatter.converter = time.gmtime
     # add console handler
-    console_handler = logging.StreamHandler()
+    console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(CONSOLE_LOG_LEVEL)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+    
+    # add error handler
+    error_handler = logging.StreamHandler()
+    error_handler.setLevel(logging.ERROR)
+    error_handler.setFormatter(formatter)
+    logger.addHandler(error_handler)
 else:
     logger.info('Logger already initizalized. Resuming normal operation.')
