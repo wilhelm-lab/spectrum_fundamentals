845bab0ed4cda2993cddeb19cfb1031bf78c9379
ge43vadgit config --global user.email you@example.com <ge43vad@mytum.de>
2022-09-05 14:37:14 +0200

    fixing maxquant_to_internal

--- fundamentals/constants.py
+++ fundamentals/constants.py
@@ -91,7 +91,7 @@ MAXQUANT_VAR_MODS = {
     "K(Lys8)": "K[UNIMOD:259]",
     "R(Arg10)": "R[UNIMOD:267]",
     "C(Carbamidomethyl (C))": "C[UNIMOD:4]",
-    "_(ac)": "_[UNIMOD:1]-",
+    "^_(ac)": "_[UNIMOD:1]-",
 }
 
 MAXQUANT_NC_TERM = {
