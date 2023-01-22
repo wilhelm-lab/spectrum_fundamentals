8cab60161bc57d09215ecf942e6cd81dbc643c3e
Mario Picciani <mario.picciani@tum.de>
2023-01-22 15:13:27 +0100

    Bump version from 0.1.0 to 0.1.1

--- spectrum_fundamentals/__init__.py
+++ spectrum_fundamentals/__init__.py
@@ -1,7 +1,7 @@
 """Initialize fundamentals."""
 __author__ = "Victor Giurcoiu"
 __email__ = "victor.giurcoiu@tum.de"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import logging
 import logging.handlers
