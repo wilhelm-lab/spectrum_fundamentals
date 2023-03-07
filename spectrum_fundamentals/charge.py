5abc275a786d42e5265cf910a6f92415c3f72175
Mario Picciani <mario.picciani@tum.de>
2023-03-07 13:15:11 +0100

    fixed pre-commit hooks

--- spectrum_fundamentals/charge.py
+++ spectrum_fundamentals/charge.py
@@ -1,13 +1,12 @@
-from typing import List, Union, Optional
+from typing import List, Optional, Union
 
 import numpy as np
-
 
 
 def indices_to_one_hot(labels: Union[int, List[int], np.ndarray], classes: Optional[int] = None) -> np.ndarray:
     """
     Convert a single or a list of labels to one-hot encoding.
-    
+
     :param labels: The labels to be one-hot encoding. Must be zero-based.
     :param classes: The number of classes, i.e. the length of the encoding. If omitted, set to the max label + 1.
 
@@ -16,20 +15,21 @@ def indices_to_one_hot(labels: Union[int, List[int], np.ndarray], classes: Optio
 
     :return: np.ndarray with the one-hot encoded labels.
     """
-        
     if isinstance(labels, int):
         labels = np.array([labels])
     elif isinstance(labels, (list, np.ndarray)):
         labels = np.array(labels)
     else:
-        raise TypeError(f"Type of labels not understood. Only int, List[int] and np.ndarray are supported. Given: {type(labels)}.")
-    
+        raise TypeError(
+            f"Type of labels not understood. Only int, List[int] and np.ndarray are supported. Given: {type(labels)}."
+        )
+
     max_label = labels.max()
     if classes is None:
         classes = max_label + 1
     if max_label >= classes:
         raise ValueError("All labels must be smaller to the number of classes.")
-    
+
     one_hot = np.zeros((labels.size, classes), dtype=int)
     one_hot[np.arange(labels.size), labels] = 1
 
