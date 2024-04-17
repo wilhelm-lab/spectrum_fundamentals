46e92ae7ef3ff0d03707053277cb04b8d079327b
Mostafa Kalhor <mostafa.kalhor@tum.de>
2024-04-17 09:22:53 +0000

    fixed precommit error

--- spectrum_fundamentals/metrics/similarity.py
+++ spectrum_fundamentals/metrics/similarity.py
@@ -62,7 +62,7 @@ class SimilarityMetrics(Metric):
         if charge != 0:
             if not 1 <= charge <= 5:
                 raise ValueError("Charge must be between 1 to 5.")
-            masks = constants.MASK_DICT_XL if xl else constants.MASK_DICT  
+            masks = constants.MASK_DICT_XL if xl else constants.MASK_DICT
             boolean_array = masks[charge]
             boolean_array = scipy.sparse.csr_matrix(boolean_array)
             observed_intensities = scipy.sparse.csr_matrix(observed_intensities)
@@ -217,7 +217,7 @@ class SimilarityMetrics(Metric):
         if charge != 0:
             if not 1 <= charge <= 5:
                 raise ValueError("Charge must be between 1 to 5.")
-            masks = constants.MASK_DICT_XL if xl else constants.MASK_DICT  
+            masks = constants.MASK_DICT_XL if xl else constants.MASK_DICT
             boolean_array = masks[charge]
             boolean_array = scipy.sparse.csr_matrix(boolean_array)
             observed_intensities_array = observed_intensities.multiply(boolean_array).toarray()
@@ -478,8 +478,8 @@ class SimilarityMetrics(Metric):
         )
         self.metrics_val[f"mse{key_suffix}"] = SimilarityMetrics.abs_diff(true_intensities, pred_intensities, "mse")
         self.metrics_val[f"modified_cosine{key_suffix}"] = SimilarityMetrics.modified_cosine(
-                true_intensities, pred_intensities, self.mz, self.mz
-            )
+            true_intensities, pred_intensities, self.mz, self.mz
+        )
 
         col_names_spectral_angle = [
             f"spectral_angle_{amount}_charge{key_suffix}" for amount in ["single", "double", "triple"]
@@ -494,14 +494,13 @@ class SimilarityMetrics(Metric):
             f"spearman_corr_{amount}_charge{key_suffix}" for amount in ["single", "double", "triple"]
         ] + [f"spearman_corr_b_ions{key_suffix}", f"spearman_corr_y_ions{key_suffix}"]
 
-
         if key_suffix != "":
             # dirty fix, if the key_suffix is not "", that means we have XL mode.
             # TODO: fix self.mz for XL mode
-            
+
             self.metrics_val[f"spearman_corr{key_suffix}"] = SimilarityMetrics.correlation(
                 true_intensities, pred_intensities, 0, "spearman", xl=True
-                )
+            )
 
             for i, col_name_spectral_angle in enumerate(col_names_spectral_angle):
                 self.metrics_val[col_name_spectral_angle] = SimilarityMetrics.spectral_angle(
@@ -512,7 +511,7 @@ class SimilarityMetrics(Metric):
                 self.metrics_val[col_name_pearson_corr] = SimilarityMetrics.correlation(
                     true_intensities, pred_intensities, i + 1, "pearson", xl=True
                 )
-                
+
             for i, col_name_spearman_corr in enumerate(col_names_spearman_corr):
                 self.metrics_val[col_name_spearman_corr] = SimilarityMetrics.correlation(
                     true_intensities, pred_intensities, i + 1, "spearman", xl=True
@@ -531,15 +530,8 @@ class SimilarityMetrics(Metric):
                 self.metrics_val[col_name_pearson_corr] = SimilarityMetrics.correlation(
                     true_intensities, pred_intensities, i + 1, "pearson"
                 )
-                
+
             for i, col_name_spearman_corr in enumerate(col_names_spearman_corr):
                 self.metrics_val[col_name_spearman_corr] = SimilarityMetrics.correlation(
                     true_intensities, pred_intensities, i + 1, "spearman"
                 )
-
-
-            
-        
-        
-
-        
